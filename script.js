4f6d3f9a1ecc0421a9ecd5539e2b0680ee5fa24d
Aline Akemi Nishida <keminishida@gmail.com>
2024-10-08 13:02:13 -0300

    Correções de responsividade

--- script.js
+++ script.js
@@ -24,3 +24,24 @@ document.querySelectorAll('.menu-desktop ul li a').forEach(item => {
     });
 });
 
+// Seleciona o botão de menu e o menu desktop
+const menuToggle = document.querySelector('.menu-toggle');
+const menuDesktop = document.querySelector('.menu-desktop');
+const menuClose = document.querySelector('.menu-close');
+
+// Evento para abrir o menu
+menuToggle.addEventListener('click', () => {
+    menuDesktop.classList.add('active');
+});
+
+// Evento para fechar o menu
+menuClose.addEventListener('click', () => {
+    menuDesktop.classList.remove('active');
+});
+
+// Opcional: adicionar um evento de clique fora do menu para fechá-lo
+document.addEventListener('click', (event) => {
+    if (!menuDesktop.contains(event.target) && !menuToggle.contains(event.target)) {
+        menuDesktop.classList.remove('active');
+    }
+});
